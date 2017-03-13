bdbc67b9b6872686b3c4b7d7fa14598107eda34d
evertonnrb <everton.nrb@hotmail.com>
2017-03-13 16:25:48 -0400

    Iniciando rest

---
+++ src/java/service/GenericResource.java
@@ -0,0 +1,66 @@
+
+package service;
+
+import com.google.gson.Gson;
+import entidade.Usuario;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author everton
+ */
+@Path("Usuario")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of service.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/text")
+    public String getXml() {
+        //TODO return proper representation object
+        //throw new UnsupportedOperationException();
+        return "Testando....";
+    }
+    
+    @GET
+    @Produces("application/json")
+    @Path("Usuario/get")
+    public String getNomeUsuario(){
+        
+        Usuario usu = new Usuario();
+        usu.setNome("Notreve");
+        usu.setEmail("notreve@not.com");
+        usu.setSenha("1221211221");
+        usu.setPerfil("ADM");
+        Gson g = new Gson();
+        return g.toJson(usu);
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
